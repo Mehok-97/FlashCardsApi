e580768bb98621d840b511e5bb7600a5cbfade6c
Mehok-97 <maherakkari1@gmail.com>
2021-12-29 11:35:06 +0200

    change The Ids to 1,2,3,4 + write in arabic

--- Migrations/20211228143217_addProducts.cs
+++ Migrations/20211228143217_addProducts.cs
@@ -8,10 +8,10 @@ namespace FlashCardsApi.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             
-            migrationBuilder.Sql("INSERT INTO Products VALUES('House','manzel',CURRENT_TIMESTAMP,DATEADD(second,10,CURRENT_TIMESTAMP),10,450.5,2)");
-    	    migrationBuilder.Sql("INSERT INTO Products VALUES('Mobile','Jawmal',CURRENT_TIMESTAMP,DATEADD(second,20,CURRENT_TIMESTAMP),10,120,3)");
-            migrationBuilder.Sql("INSERT INTO Products VALUES('DataEntry','edkhal',CURRENT_TIMESTAMP,DATEADD(second,30,CURRENT_TIMESTAMP),10,0,1)");
-            migrationBuilder.Sql("INSERT INTO Products VALUES('Bicycle','pisklet',CURRENT_TIMESTAMP,DATEADD(second,40,CURRENT_TIMESTAMP),10,40.5,4)");
+            migrationBuilder.Sql("INSERT INTO Products VALUES('House','المنزل',CURRENT_TIMESTAMP,DATEADD(second,10,CURRENT_TIMESTAMP),10,450.5,2)");
+    	    migrationBuilder.Sql("INSERT INTO Products VALUES('Mobile','الهاتف',CURRENT_TIMESTAMP,DATEADD(second,20,CURRENT_TIMESTAMP),10,120,3)");
+            migrationBuilder.Sql("INSERT INTO Products VALUES('DataEntry','إدخال بيانات',CURRENT_TIMESTAMP,DATEADD(second,30,CURRENT_TIMESTAMP),10,0,1)");
+            migrationBuilder.Sql("INSERT INTO Products VALUES('Bicycle','دراجة هوائية',CURRENT_TIMESTAMP,DATEADD(second,40,CURRENT_TIMESTAMP),10,40.5,4)");
 
         }
 
--- Migrations/20211228202420_CustomDetails.cs
+++ Migrations/20211228202420_CustomDetails.cs
@@ -8,28 +8,28 @@ namespace FlashCardsApi.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Size','250',6)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Old','25',6)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Rooms','4',6)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Site','Syria',6)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Size','250',1)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Old','25',1)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Rooms','4',1)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Site','Syria',1)");
 
 
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Kind','Samsung',7)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('OS','Andoid.11',7)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Color','Brown',7)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Cameras','3',7)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Kind','Samsung',2)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('OS','Andoid.11',2)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Color','Brown',2)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Cameras','3',2)");
 
 
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Hours','8',8)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Type','FullTime',8)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Vaccations','11',8)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Team','Yes',8)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Hours','8',3)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Type','FullTime',3)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Vaccations','11',3)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Team','Yes',3)");
 
 
 
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Color','Yellow',9)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Material','Iron',9)");
-            migrationBuilder.Sql("INSERT INTO Details VALUES('Extras','Helmet',9)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Color','Yellow',4)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Material','Iron',4)");
+            migrationBuilder.Sql("INSERT INTO Details VALUES('Extras','Helmet',4)");
             
 
         }
